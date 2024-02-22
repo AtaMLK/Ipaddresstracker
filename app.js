c9d04317fe7ec4ad0075e96504ebb6707db20ee8
AtaMLK <molkaraeeata@gmail.com>
2024-02-22 23:26:15 +0300

    initialized

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
-"use strict"
+"use strict";
+/* 
+const { LatLng } = require("leaflet"); */
 
+let myLocation = navigator.geolocation;
+console.log(myLocation);
+
+let map = L.map("map").setView([myLocation.lat, myLocation.lng], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+//additional options that can placed on the map
+/* var marker = L.marker([51.5, -0.09]).addTo(map);
+
+var circle = L.circle([51.508, -0.11], {
+  color: "red",
+  fillColor: "#f03",
+  fillOpacity: 0.5,
+  radius: 500,
+}).addTo(map);
+
+var polygon = L.polygon([
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047],
+]).addTo(map); */
