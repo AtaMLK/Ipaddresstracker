5721dc5630077e60cf2ec7d21cb585b7beb1dda6
AtaMLK <molkaraeeata@gmail.com>
2024-03-05 23:26:19 +0300

    map and API coding

--- app.js
+++ app.js
@@ -8,29 +8,55 @@ const ispName = document.querySelector(".isp");
 const serachedIp = document.querySelector(".search-bar");
 const searchedButton = document.querySelector(".search-btn");
 
-const API_KEY = "at_wsS8PAPWhRErLBy942MlMMFermjPj";
-const postitionCoords = [];
-const data = [];
+const myIp = "78.174.26.241";
+const API_KEY = "at_OXooYfHJxMAdvCndpkbl0AOxrtnND";
+const URL = `https://geo.ipify.org/api/v2/country,city?apiKey=${/* API_KEY */}` /* 
+const postitionCoords = []; */ /* 
+  const url = 'https://apiip.net/api/check?ip='&accessKey='+ accessKey;  */
+let ip;
+let ipLoc;
+let ipUtc;
+let ipIsp;
 /* ================== User Location Finder ================== */
-navigator.geolocation.getCurrentPosition(function locationFinder(pos) {
+/* navigator.geolocation.getCurrentPosition(function locationFinder(pos) {
   postitionCoords[0] = pos.coords.latitude;
   postitionCoords[1] = pos.coords.longitude;
   postitionCoords[2] = pos.coords.accuracy;
-});
+}); */
+
 /* ================== API reader  ================== */
+
 async function ipadress() {
-  const response = await fetch(
-    `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}`
-  ); /* .then(response=> response.json()) */
-  const data = await response.json();
-  console.log(data);
+  const response = await fetch(URL).then((res) => res.json());
+  console.log(response);
+  let data = await response;
   if (!data) {
-    throw new Error(`${response.status} : ${await response.statusText()}`);
+    throw new Error(`${response.status} : ${response.statusText}`);
   }
+  return data;
 }
-/* ipadress(); */
+ipadress();
+
+ipadress()
+  .then((data) => {
+    console.log(data);
+    const { city, country, geonameId } = data.location;
+
+    ip = data.ip;
+    ipIsp = data.isp;
+    ipUtc = data.location.timezone;
+
+    ipAddress.textContent = ip;
+    ipLocation.textContent = `${city}  ${country} ${geonameId}`;
+    utcTimezone.textContent = `${ipUtc} UTC`;
+    ispName.textContent = `${ipIsp} ${data.as.name}`;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
 /* ================== MAP  ================== */
-let map = L.map("map").setView([41.01526, 28.9597], 15);
+let map = L.map("map").setView([myLat, myLng], 17);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
@@ -39,4 +65,4 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 /* ================== MAP  Options ================== */
-L.marker([41.01526, 28.9597]).addTo(map).bindPopup("here you are").openPopup();
+L.marker([myLat, myLng]).addTo(map).bindPopup("here you are").openPopup();
--- dist/index.7c0ccee6.js
+++ dist/index.7c0ccee6.js
@@ -1,28 +1,35 @@
 "use strict";
-/* 
-const { LatLng } = require("leaflet"); */ let myLocation = navigator.geolocation;
-console.log(myLocation);
-let map = L.map("map").setView([
-    myLocation.lat,
-    myLocation.lng
-], 13);
+/* ================== Variables ================== */ const ipAddress = document.querySelector(".ipaddress");
+const ipLocation = document.querySelector(".location");
+const utcTimezone = document.querySelector(".timezone");
+const ispName = document.querySelector(".isp");
+const serachedIp = document.querySelector(".search-bar");
+const searchedButton = document.querySelector(".search-btn");
+const API_KEY = "at_wsS8PAPWhRErLBy942MlMMFermjPj";
+const postitionCoords = [];
+const data = [];
+/* ================== User Location Finder ================== */ navigator.geolocation.getCurrentPosition(function locationFinder(pos) {
+    postitionCoords[0] = pos.coords.latitude;
+    postitionCoords[1] = pos.coords.longitude;
+    postitionCoords[2] = pos.coords.accuracy;
+});
+/* ================== API reader  ================== */ async function ipadress() {
+    const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}`); /* .then(response=> response.json()) */ 
+    const data = await response.json();
+    console.log(data);
+    if (!data) throw new Error(`${response.status} : ${await response.statusText()}`);
+}
+/* ipadress(); */ /* ================== MAP  ================== */ let map = L.map("map").setView([
+    41.01526,
+    28.9597
+], 15);
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map); //additional options that can placed on the map
- /* var marker = L.marker([51.5, -0.09]).addTo(map);
-
-var circle = L.circle([51.508, -0.11], {
-  color: "red",
-  fillColor: "#f03",
-  fillOpacity: 0.5,
-  radius: 500,
 }).addTo(map);
-
-var polygon = L.polygon([
-  [51.509, -0.08],
-  [51.503, -0.06],
-  [51.51, -0.047],
-]).addTo(map); */ 
+/* ================== MAP  Options ================== */ L.marker([
+    41.01526,
+    28.9597
+]).addTo(map).bindPopup("here you are").openPopup();
 
 //# sourceMappingURL=index.7c0ccee6.js.map
